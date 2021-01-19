d4bb9b1d670dc85e7d31c1e96f9662a812644ee9
DarthCorvidus <github@vm01.telton.de>
2021-01-19 21:13:33 +0100

    Actually forgot composer.json in last push. Added autoload as well.

--- tests/autoload.php
+++ tests/autoload.php
@@ -3,3 +3,10 @@ require './src/Validate.php';
 require './src/ValidateException.php';
 require './src/ValidateDate.php';
 require './src/ValidateTime.php';
+if(file_exists("../plibv4-assert")) {
+	require "../plibv4-assert/src/Assert.php";
+}
+
+if(file_exists("./vendor/")) {
+	require "./vendor/autoload.php";
+}
