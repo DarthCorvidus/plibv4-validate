4a8ac65c2e62bd6c90607a1d24ea4e0b6cb74faf
DarthCorvidus <github@vm01.telton.de>
2021-01-19 21:23:02 +0100

    Wrote some comments on tests, also added one test (time with more than three segments should be rejected).

--- tests/ValidateTimeTest.php
+++ tests/ValidateTimeTest.php
@@ -20,74 +20,120 @@ class ValidateTimeTest extends TestCase {
 		$this->expectException(InvalidArgumentException::class);
 		$validate = new ValidateTime(-1);
 	}
-
+	/**
+	 * If 'strict' time with leading zeroes valid
+	 */
 	public function testValidateStrict() {
 		$validate = new ValidateTime();
 		$this->assertEquals(NULL, $validate->validate("05:37:01"));
 	}
 	
+	/**
+	 * If 'sloppy' time without leading zeroes valid
+	 */
 	public function testValidateSloppy() {
 		$validate = new ValidateTime();
 		$this->assertEquals(NULL, $validate->validate("5:37:1"));
 	}
 
+	/**
+	 * If time without seconds is valid
+	 */
 	public function testValidateHoursMinutes() {
 		$validate = new ValidateTime();
 		$this->assertEquals(NULL, $validate->validate("05:37"));
 
 	}
 
+	/**
+	 * If time without minutes is valid
+	 */
 	public function testValidateHours() {
 		$validate = new ValidateTime();
 		$this->assertEquals(NULL, $validate->validate("05"));
 	}
 
+	/**
+	 * If complete bogus is rejected
+	 */
 	public function testBogus() {
 		$time = new ValidateTime();
 		$this->expectException(ValidateException::class);
 		$time->validate("Bogus");
 	}
 
+	/**
+	 * If empty hours (:MM:SS) are rejected
+	 */
 	public function testEmptyHours() {
 		$time = new ValidateTime();
 		$this->expectException(ValidateException::class);
 		$time->validate(":47:30");
 	}
 
+	/**
+	 * If empty minutes are rejected (HH::SS)
+	 */
 	public function testEmptyMinutes() {
 		$time = new ValidateTime();
 		$this->expectException(ValidateException::class);
 		$time->validate("07::30");
 	}
 
+	/**
+	 * If empty seconds are rejected (HH:MM:)
+	 */
 	public function testEmptySeconds() {
 		$time = new ValidateTime();
 		$this->expectException(ValidateException::class);
 		$time->validate("07:47:");
 	}
 	
+	/**
+	 * If out of range hours are rejected (only if limit is set to 
+	 * ValidateTime::DAY)
+	 */
 	public function testHoursOutOfRange() {
 		$validate = new ValidateTime(ValidateTime::DAY);
 		$this->expectException(ValidateException::class);
 		$validate->validate("25");
 	}
 
+	/**
+	 * If out of range hours are allowed (only if limit is set to 
+	 * ValidateTime::UNLIMITED)
+	 */
 	public function testHoursUnlimited() {
 		$validate = new ValidateTime(ValidateTime::UNLIMITED);
 		$this->assertEquals(NULL, $validate->validate("125:02:47"));
 	}
 
-	
+	/**
+	 * If out of range minutes are rejected (always)
+	 */
 	public function testMinutesOutOfRange() {
 		$validate = new ValidateTime();
 		$this->expectException(ValidateException::class);
 		$validate->validate("22:60");
 	}
 
+	/**
+	 * If out of range seconds are rejected (always)
+	 */
 	public function testSecondsOutOfRange() {
 		$validate = new ValidateTime();
 		$this->expectException(ValidateException::class);
 		$validate->validate("22:39:72");
 	}
+	
+	/**
+	 * If too many segments are rejected (??:HH:MM:SS)
+	 */
+	public function testTooMany() {
+		$validate = new ValidateTime();
+		$this->expectException(ValidateException::class);
+		$validate->validate("17:22:39:72");
+	}
+
 	
 }
