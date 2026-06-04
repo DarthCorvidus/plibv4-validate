b9a745a5898572c28a29f488082658eda0a12b66
DarthCorvidus <github@vm01.telton.de>
2026-06-04 18:44:23 +0200

    chore: removed DocBlock

--- src/ValidateInteger.php
+++ src/ValidateInteger.php
@@ -12,9 +12,6 @@ final class ValidateInteger implements Validate {
 	}
 	#[\Override]
 	public function validate(string $validee): void {
-		/**
-		 * 
-		 */
 		if(preg_match("/^[0-9]+$/", $validee)) {
 			return;
 		}
